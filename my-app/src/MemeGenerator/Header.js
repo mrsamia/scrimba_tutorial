d1fd229511aab6d4bd66495549583a3fad4a127e
mrsamia <15203060@iubat.edu>
2022-03-22 17:48:28 +0600

    SRM | meme

---
+++ my-app/src/MemeGenerator/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './memeGenerator.css';
+
+function Header(props) {
+    return (
+        <div className='meme_nav'>
+            <img className='imagee' src={require('../images/Vector.png')} />
+            <h1 className='h1_txt'>Meme Generator</h1>
+            <h4 className='h4_txt'>React Course -3</h4>
+        </div>
+
+    );
+}
+
+export default Header;
