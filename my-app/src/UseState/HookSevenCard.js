ab134ca3f2f50db06fb2976422b1867de49d89e7
mrsamia <15203060@iubat.edu>
2022-04-05 19:42:53 +0600

    SRM | HookSeven

--- my-app/src/UseState/HookSevenCard.js
+++ my-app/src/UseState/HookSevenCard.js
@@ -3,27 +3,33 @@ import './hookCard.css';
 
 function HookSevenCard(props) {
 
-    const [content,setContent]=useState({
-        name:"Samia Rahman",
-        phone:"+1 (719) 555-1212",
+    const [content, setContent] = useState({
+        name: "Samia Rahman",
+        phone: "+1 (719) 555-1212",
         mail: "itsmyreyfalname@examaple.com",
-        isFavorite: true
+        isFavorite: false
     })
-    
-// function click(){
-//     setContent(content.isFavorite ?'../images/Empty_Star.png':'../images/full_star.png')
-// }
+// let src;
+//     function click() {
+//         if (src === '../images/Empty_Star.png') {
+
+//             setContent('../images/full_star.png')
+//         }
+//         else if (src === '../images/full_star.png') {
+//             setContent('../images/Empty_Star.png')
+//         }
+//     }
 
     return (
         <div className='d-flex justify-content-center'>
-            
+
             <div className='Card'>
                 <div>
-                    <img className='imge' src={require('../images/catOne.webp')}/>
+                    <img className='imge' src={require('../images/catOne.webp')} />
                 </div>
                 <div className='pt-5'>
                     <div>
-                        <img className='star'  src={require(content.isFavorite ?'../images/Empty_Star.png':'../images/full_star.png')}/>
+                        <img className='star'  src={require(content.isFavorite ? '../images/Empty_Star.png':'../images/full_star.png')} />
                     </div>
                     <div>
                         <h5>{content.name}</h5>
