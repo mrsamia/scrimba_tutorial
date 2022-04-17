d92b967fe531c674cdc6cd094be6dd63a4f9bfdf
mrsamia <15203060@iubat.edu>
2022-04-17 21:00:10 +0600

    SRM | radio

--- my-app/src/Form/FormOne.js
+++ my-app/src/Form/FormOne.js
@@ -9,7 +9,8 @@ function FormOne(props) {
         lastname: " ",
         address: " ",
         email: " ",
-        textarea: " "
+        textarea: " ",
+        language: " "
     })
 
     function onchnageHandler(event) {
@@ -32,9 +33,23 @@ function FormOne(props) {
                 <input type="text" name="address" placeholder='Your address' onChange={onchnageHandler} /><br></br><br></br>
                 <input type="text" name="email" placeholder='Your email' onChange={onchnageHandler} /><br></br><br></br>
             </form>
-
+            <h4>Iext Area</h4>
             <p>{value.textarea}</p>
             <textarea type="text" name="textarea" placeholder='Text Area' onChange={onchnageHandler} />
+
+            <fieldset>
+                <h4>Radio Button</h4>
+                <p>Choose your favorite Web language:</p>
+
+                <input type="radio" id='html' name='language' value="html" />
+                <label htmlFor="html">HTML</label><br></br>
+
+                <input type="radio" id='css' name='language' value="css" />
+                <label htmlFor="css">CSS</label><br></br>
+
+                <input type="radio" id='react' name='language' value="react" />
+                <label htmlFor="react">React</label>
+            </fieldset>
 
         </div>
     );
